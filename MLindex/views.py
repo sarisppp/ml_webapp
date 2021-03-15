28ea377fccc7b48b92c87e1d960f5f2d7cae90d7
sarisppp <saris.ppp@gmail.com>
2021-03-15 20:01:45 +0700

    forecast up hold down

--- MLindex/views.py
+++ MLindex/views.py
@@ -20,9 +20,9 @@ def Hello(request):
 def signal(datatrain,periods=16):
     datatrain['output']=0
     for index,row in datatrain.iterrows():
-        if row['MACD13']>row['Signal'] and row['RSI14']>row['EMAVRSI13'] and row['RSI14']<70:
+        if row['MACD13']>row['Signal'] and row['RSI14']>row['EMAVRSI13']+(row['EMAVRSI13']*0.2) and row['RSI14']<70:
             signalPre=1
-        elif row['MACD13']<row['Signal'] and row['RSI14']<row['EMAVRSI13'] and row['RSI14']>30:
+        elif row['MACD13']<row['Signal'] and row['RSI14']<row['EMAVRSI13']-(row['EMAVRSI13']*0.2) and row['RSI14']>30:
             if row['Close']<row['EMAV']:
                 signalPre=-1
             else:
@@ -56,12 +56,6 @@ def predict(data,df):
   
     return df
 def buy_hole_sell(data_update2):
-        data_update2['pricebuy']=0
-        data_update2['pricesell']=0
-        data_update2['macdbuy']=0
-        data_update2['macdsell']=0
-        data_update2['rsibuy']=0
-        data_update2['rsisell']=0
         data_update2['Alert']=0
         data_update2=data_update2.astype(float)
         status=0
@@ -69,124 +63,35 @@ def buy_hole_sell(data_update2):
             if status==0:
                 if row['signal_predict']==1:
                     status=1
-                    close=row['Close']
-                    buy=0
-                    pricebuy=close
-                    pricesell=0
-                    sell=0
                     Alert=1
-                    macdbuy=row['MACD13']
-                    macdsell=0
-                    rsibuy=row['RSI14']
-                    rsisell=0
                 elif row['signal_predict']==-1:
                     status=-1
-                    close=row['Close']
-                    pricebuy=0
-                    pricesell=close
-                    buy=0
-                    sell=0
                     Alert=-1
-                    macdbuy=0
-                    macdsell=row['MACD13']
-                    rsibuy=0
-                    rsisell=row['RSI14']
                 else:
                     status=0
-                    buy=0
-                    sell=0
                     Alert=0
-                    pricebuy=0
-                    pricesell=0
-                    macdbuy=0
-                    macdsell=0
-                    rsibuy=0
-                    rsisell=0
             else :
                 if status==1:
                     if row['signal_predict']==1:
-                        buy=0
-                        sell=0
-                        pricebuy=0
-                        pricesell=0
                         Alert=0
-                        macdbuy=0
-                        macdsell=0
-                        rsibuy=0
-                        rsisell=0
                     elif row['signal_predict']==-1:
                         status=-1
-                        sell=row['Close']-close
-                        close=row['Close']
-                        pricebuy=0
-                        pricesell=close
-                        buy=0
                         Alert=-1
-                        macdbuy=0
-                        macdsell=row['MACD13']
-                        rsibuy=0
-                        rsisell=row['RSI14']
                     else:
-                        buy=0
-                        sell=0
                         Alert=0
-                        pricebuy=0
-                        pricesell=0
-                        macdbuy=0
-                        macdsell=0
-                        rsibuy=0
-                        rsisell=0
                 elif status==-1:
                     if row['signal_predict']==1:
                         status=1
-                        buy=close-row['Close']
-                        close=row['Close']
-                        sell=0
                         Alert=1
-                        pricebuy=close
-                        pricesell=0
-                        macdbuy=row['MACD13']
-                        macdsell=0
-                        rsibuy=row['RSI14']
-                        rsisell=0
                     elif row['signal_predict']==-1:
-                        buy=0
-                        sell=0
                         Alert=0
-                        pricebuy=0
-                        pricesell=0
-                        macdbuy=0
-                        macdsell=0
-                        rsibuy=0
-                        rsisell=0
                     else:
-                        buy=0
-                        sell=0
                         Alert=0
-                        pricebuy=0
-                        pricesell=0
-                        macdbuy=0
-                        macdsell=0
-                        rsibuy=0
-                        rsisell=0
                 else:
-                    buy=0
-                    sell=0
-                    price=0
-                    pricebuy=0
-                    pricesell=0
-                    macdbuy=0
-                    macdsell=0
-                    rsibuy=0
-                    rsisell=0
+                   Alert=0
             data_update2.at[index,'Alert']=Alert
-            data_update2.at[index,'pricebuy']=pricebuy
-            data_update2.at[index,'pricesell']=pricesell
-            data_update2.at[index,'macdbuy']=macdbuy
-            data_update2.at[index,'macdsell']=macdsell
-            data_update2.at[index,'rsibuy']=rsibuy
-            data_update2.at[index,'rsisell']=rsisell
         return data_update2
+
 def hello(request):
     used_features = ["Timestamp","Close","EMAV","RSI14","MACD13","EMAVRSI13","Signal"]
     df = pd.read_csv("Set50_20190314_20200820_1minute.csv",usecols =used_features,encoding= 'unicode_escape')
@@ -198,8 +103,8 @@ def hello(request):
     df_new.set_index("Timestamp",inplace=True)
     df_new=df_new.dropna()
     df_new=predict(df_trian,df_new)
-    df_new=buy_hole_sell(df_new.head(20))
-    df_new=df_new.head(20)
+    df_new=buy_hole_sell(df_new.tail(200))
+    df_new=df_new.head(200)
     print("===============step 2======================")
 
     json_records = df_new.reset_index().to_json(orient ='records') 
